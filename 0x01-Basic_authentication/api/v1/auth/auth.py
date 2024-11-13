a3e508c1f1f86c8ac55418775ea184c284efde33
Temitayo <afolabi.t@klakpad.com>
2024-11-13 22:39:31 +0100

    add: Authentication class and initialize public methods

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Auth module for API Authentication
+"""
+from typing import List, TypeVar
+from flask import request
+
+class Auth():
+    """
+    Auth class for authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        It determines if authentication is required for a gicen path
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the Authorization header from the request
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request
+        """
+        return None
