736a347b97a47841e9d24699719cba402d13e0c5
Temitayo <afolabi.t@klakpad.com>
2024-11-20 21:26:28 +0100

    add: user model with that use sqlalchemy ORM

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+user model module
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# Create a declarative base
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    # Define columns
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
