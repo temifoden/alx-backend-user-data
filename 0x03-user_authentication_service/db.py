a239fa98362c69f0e0adf294c594d30e28297eb0
Temitayo <afolabi.t@klakpad.com>
2024-11-21 23:40:37 +0100

    refactor: script

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,56 +1,73 @@
-#!/bin/bash/env python3
-"""DB module
+#!/usr/bin/env python3
 """
+BD class
+"""
+
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
 from user import Base, User
 
 
-class DB:
-    """DB class
-    """
+DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db")
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> str:
-        """
-        Adds a new user to the database
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add user to database
 
         Args:
-        email (str): The email of the user.
-        hashed_password (str): The hased password of the user
+            email (string): email of user
+            hashed_password (string): password of user
+        Returns:
+            User: user created
+        """
+        if not email or not hashed_password:
+            return
+        user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(user)
+        session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find user by some arguments
 
         Returns:
-        User: The created User Object
+            User: user found or raise error
         """
-        try:
-            if  not email or not hashed_password:
-                return
-            # Create a new User object
-            new_user = User(email=email, hashed_password=hashed_password)
-            # Add the user to the session
-            self._session.add(new_user)
-            # Commit the session to persist the user in the databse
-            self._session.commit()
-            # Return the new user object
-            return new_user
-        except Exception as e:
-            self._session.rollback()
-            raise e
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user
+
+        Args:
+            user_id (int): id of user
+        """
+        user = self.find_user_by(id=user_id)
+        for key, val in kwargs.items():
+            if key not in DATA:
+                raise ValueError
+            setattr(user, key, val)
+        self._session.commit()
+        return None
